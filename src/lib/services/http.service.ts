5ce9bd520c058a079a60cbb6f8b9f6f628e4a6c7
EllaiSys <admin@ellaisys.com>
2021-05-02 00:47:18 +0530

    Amit: feature: Enhanced the http service to handle form data and more content-type

--- src/lib/services/http.service.ts
+++ src/lib/services/http.service.ts
@@ -8,11 +8,23 @@ import { Observable, empty } from 'rxjs';
 import { StorageConfiguration } from '../configurations/storage.configuration';
 import { count } from 'rxjs/operators';
 
+export enum ContentType {
+    NOTHING = "_NOTHING OPTION",
+    ENCODED_FORM_DATA = "_ENCODED_FORM_OPTION",
+    JSON_DATA = "_JSON_HEADER_OPTION",
+    FORM_DATA = "_MULTIPART_MIXED_OPTION",
+    FORM_DATA_MULTIPART_NIXED = "_MULTIPART_MIXED_OPTION",
+    CUSTOM = "_CUSTOM_OPTION"
+} //Enum End
+
 const _ENCODED_FORM_OPTION: Object = {
     headers: new HttpHeaders().append('Content-Type', 'application/x-www-form-urlencoded')
 };
 const _JSON_HEADER_OPTION: Object = {
     headers: new HttpHeaders().append('Content-Type', 'application/json')
+};
+const _MULTIPART_MIXED_OPTION = {
+    headers: new HttpHeaders().append('Content-Type', 'multipart/mixed')
 };
 
 @Injectable({
@@ -77,9 +89,12 @@ export class HttpService {
      * @param encoded
      * @param onlyServerURL
      */
-    public async post<T>(_uri: string, _body: any, _encoded:boolean=false, _onlyServerURL: boolean=false, _params: Object=null){
+    public async post<T>(_uri: string, _body: any, _onlyServerURL: boolean=false, _params: Object=null, _contentType: ContentType=ContentType.JSON_DATA, _contentTyeCustom: string=null){
+        //Set URL endpoint
         let url: string = ((_onlyServerURL)?HttpConfiguration.server:this.endpoint) + _uri;
-        let options: Object = (_encoded)?_ENCODED_FORM_OPTION:_JSON_HEADER_OPTION;
+
+        //Set Content type option
+        let options: Object = this.getContentTypeOptions(_contentType, _contentTyeCustom);
 
         //Add params if exists
         if (_params) {
@@ -156,6 +171,42 @@ export class HttpService {
         return await this._http.patch<T>(url, _body);
     } //Function ends
 
+
+    /**
+     * Configure the Content Type
+     * 
+     * @param _contentType 
+     * @param _contentTyeCustom 
+     */
+    private getContentTypeOptions(_contentType: ContentType=ContentType.JSON_DATA, _contentTyeCustom: string=null): Object {
+        let objReturnValue: Object;
+        try {
+            //Configure Options params
+            switch (_contentType) {
+                case ContentType.NOTHING:
+                    objReturnValue={};
+                    break;
+                case ContentType.ENCODED_FORM_DATA:
+                    objReturnValue = _ENCODED_FORM_OPTION;
+                    break;
+                case ContentType.FORM_DATA:
+                case ContentType.FORM_DATA_MULTIPART_NIXED:
+                    objReturnValue = _MULTIPART_MIXED_OPTION;
+                    break;
+                case ContentType.CUSTOM:
+                    objReturnValue = { headers: new HttpHeaders().append('Content-Type', _contentTyeCustom) };
+                    break;
+                case ContentType.JSON_DATA:
+                default:
+                    objReturnValue = _JSON_HEADER_OPTION;
+                    break;
+            } //End switch
+        } catch (error) {
+            objReturnValue={};
+        } //Try-catch ends
+        return objReturnValue;
+    } //Function ends
+
 } //Class ends
 
 
@@ -177,11 +228,11 @@ export class RequestInterceptor implements HttpInterceptor {
 
             let boolNeedsAuthentication: boolean = !(req.url.search('googleapis.com')>0);
             if (boolNeedsAuthentication) {
-                if (!req.headers.has('Content-Type')) {
-                    req = req.clone({ 
-                        headers: req.headers.set('Content-Type', 'application/json') 
-                    });
-                } //End if
+                // if (!req.headers.has('Content-Type')) {
+                //     req = req.clone({ 
+                //         headers: req.headers.set('Content-Type', 'application/json') 
+                //     });
+                // } //End if
 
                 if (_AUTH_TOKEN) {
                     req = req.clone({ 
